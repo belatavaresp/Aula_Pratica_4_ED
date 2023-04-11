a84d1dc81b604fb25830952cbcffeae5db0e9b86
Izabela Tavares <belabh2003yahoo@gmail.com>
2023-04-11 15:35:33 -0300

    pilha structured

---
+++ include/pilha.h
@@ -0,0 +1,9 @@
+//criapilha, pilhavazia, empilha, desempilha e destroipilha 
+class Pilha{
+public:
+    void criapilha();
+    bool pilhavazia();
+    void empilha(int);
+    void desempilha();
+    void destroipilha();
+};
---
+++ src/pilha.cpp
@@ -0,0 +1,22 @@
+#include "pilha.h"
+
+
+void Pilha::criapilha(){
+
+}
+
+bool Pilha::pilhavazia(){
+
+}
+
+void Pilha::empilha(int n){
+
+}
+
+void Pilha::desempilha(){
+
+}
+
+void Pilha::destroipilha(){
+    
+}
